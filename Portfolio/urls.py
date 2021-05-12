34320e5c0dea0c84d3080df899dd305fe63958a1
unknown <s-sangeeth-k@sicis.co.jp>
2021-05-12 16:27:03 +0900

    changed name and created CV homepage

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', include('registration.urls') ),
     path('', include('homepage.urls') ),
+    path('CV', include('CV.urls') ),
 ]
