1fd235e5d5995208f2dc725be647302ef35bb287
unknown <s-sangeeth-k@sicis.co.jp>
2021-11-25 17:20:55 +0900

    since old file, changed base directory in settings 46

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'Portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
