021ffe70138cc71b0e8a035bcc20b2edc45d9919
unknown <s-sangeeth-k@sicis.co.jp>
2021-11-25 17:15:23 +0900

    since old file, changed base directory in settings 4

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -129,18 +129,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
 
 #MEDIA
 MEDIA_ROOT = MEDIA_DIR
